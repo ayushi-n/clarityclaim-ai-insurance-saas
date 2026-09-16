67025787ed2b61736095d703c86cb90314d63581
Ayushi N <ayushi.naveen1@gmail.com>
2026-09-16 21:58:28 +0530

    add prisma generate and change code for db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,11 +1,19 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
-  });
+  (process.env.DATABASE_URL
+    ? new PrismaClient()
+    : (new Proxy({} as PrismaClient, {
+        get: () =>
+          new Proxy(() => {}, {
+            get: () => () => Promise.resolve(null),
+            apply: () => Promise.resolve(null),
+          }),
+      }) as unknown as PrismaClient))
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
